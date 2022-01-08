e803ddbf9188ef7186a24dd1985717a9726ccb44
Leo Foord-Kelcey <73722423+leofk@users.noreply.github.com>
2022-01-08 19:33:19 +0200

    Update testFills.cpp

--- testFills.cpp
+++ testFills.cpp
@@ -105,157 +105,156 @@ TEST_CASE("colorPicker::basic gradient","[weight=1][part=colorPicker]"){
     REQUIRE(result == expected);
 }
 
-// TEST_CASE("colorPicker::basic grid","[weight=1][part=colorPicker]"){
-//     HSLAPixel px1;
-//     px1.h = 120; px1.s = 1.0; px1.l = 0.25;
-//     gridColorPicker gridPicker(px1, FUNCTORTESTGRIDSPACING);
+TEST_CASE("colorPicker::basic grid","[weight=1][part=colorPicker]"){
+    HSLAPixel px1;
+    px1.h = 120; px1.s = 1.0; px1.l = 0.25;
+    gridColorPicker gridPicker(px1, FUNCTORTESTGRIDSPACING);
 
-//     PNG result = testColorPicker(gridPicker);
-//     result.writeToFile("images/gridColorPickerTest.png");
-//     PNG expected; expected.readFromFile("soln_images/gridColorPickerTest.png");
-//     REQUIRE(result == expected);
-// }
+    PNG result = testColorPicker(gridPicker);
+    result.writeToFile("images/gridColorPickerTest.png");
+    PNG expected; expected.readFromFile("soln_images/gridColorPickerTest.png");
+    REQUIRE(result == expected);
+}
 
-// TEST_CASE("colorPicker::basic rainbow","[weight=1][part=colorPicker]"){
-//     rainbowColorPicker rainPicker(1.0/1000.0);
+TEST_CASE("colorPicker::basic rainbow","[weight=1][part=colorPicker]"){
+    rainbowColorPicker rainPicker(1.0/1000.0);
 
-//     PNG result = testColorPicker(rainPicker);
-//     result.writeToFile("images/rainColorPickerTest.png");
-//     PNG expected; expected.readFromFile("soln_images/rainColorPickerTest.png");
-//     REQUIRE(result == expected);
-// }
+    PNG result = testColorPicker(rainPicker);
+    result.writeToFile("images/rainColorPickerTest.png");
+    PNG expected; expected.readFromFile("soln_images/rainColorPickerTest.png");
+    REQUIRE(result == expected);
+}
 
-// TEST_CASE("fill::basic solid dfs","[weight=1][part=fill]"){
-//     PNG img;
-//     img.readFromFile(SOLIDTESTIMAGE);
-//     HSLAPixel px(100., 1.0, 0.5);
+TEST_CASE("fill::basic solid dfs","[weight=1][part=fill]"){
+    PNG img;
+    img.readFromFile(SOLIDTESTIMAGE);
+    HSLAPixel px(100., 1.0, 0.5);
 
-//     animation anim;
-//     anim = filler::fillSolidDFS(img, SOLIDX, SOLIDY, px, SOLIDTOLERANCE,
-//                                       SOLIDFRAMEFREQ);
-//     PNG result = anim.write("images/dfssolid.gif"); 
-//     result.writeToFile("images/dfssolid.png");
+    animation anim;
+    anim = filler::fillSolidDFS(img, SOLIDX, SOLIDY, px, SOLIDTOLERANCE,
+                                      SOLIDFRAMEFREQ);
+    PNG result = anim.write("images/dfssolid.gif"); 
+    result.writeToFile("images/dfssolid.png");
 
-//     PNG expected; expected.readFromFile("soln_images/dfssolid.png");
-//     REQUIRE(result==expected);
+    PNG expected; expected.readFromFile("soln_images/dfssolid.png");
+    REQUIRE(result==expected);
 
-// }
+}
+
+TEST_CASE("fill::basic solid bfs","[weight=1][part=fill]"){
+    PNG img;
+    img.readFromFile(SOLIDTESTIMAGE);
+    HSLAPixel px(100., 1.0, 0.5);
+
+    animation anim;
+    anim = filler::fillSolidBFS(img, SOLIDX, SOLIDY, px, SOLIDTOLERANCE,
+                                      SOLIDFRAMEFREQ);
+    PNG result = anim.write("images/bfssolid.gif");
+    result.writeToFile("images/bfssolid.png");
+
+    PNG expected; expected.readFromFile("soln_images/bfssolid.png");
+    REQUIRE(result==expected);
+}
 
 
-// TEST_CASE("fill::basic solid bfs","[weight=1][part=fill]"){
-//     PNG img;
-//     img.readFromFile(SOLIDTESTIMAGE);
-//     HSLAPixel px(100., 1.0, 0.5);
+TEST_CASE("fill::basic grid dfs","[weight=1][part=fill]"){
+    PNG img;
+    img.readFromFile(GRIDTESTIMAGE);
+    HSLAPixel px(40., 1.0, 0.5);
 
-//     animation anim;
-//     anim = filler::fillSolidBFS(img, SOLIDX, SOLIDY, px, SOLIDTOLERANCE,
-//                                       SOLIDFRAMEFREQ);
-//     PNG result = anim.write("images/bfssolid.gif");
-//     result.writeToFile("images/bfssolid.png");
+    animation anim;
+    anim = filler::fillGridDFS(img, GRIDX, GRIDY, px, GRIDGRIDSPACING,
+                                     GRIDTOLERANCE, GRIDFRAMEFREQ);
+    PNG result = anim.write("images/dfsgrid.gif");
+    result.writeToFile("images/dfsgrid.png");
 
-//     PNG expected; expected.readFromFile("soln_images/bfssolid.png");
-//     REQUIRE(result==expected);
-// }
+    PNG expected; expected.readFromFile("soln_images/dfsgrid.png");
+    REQUIRE(result==expected);
+}
 
+TEST_CASE("fill::basic grid bfs","[weight=1][part=fill]"){
+    PNG img;
+    img.readFromFile(GRIDTESTIMAGE);
+    HSLAPixel px(200., 1.0, 0.5);
 
-// TEST_CASE("fill::basic grid dfs","[weight=1][part=fill]"){
-//     PNG img;
-//     img.readFromFile(GRIDTESTIMAGE);
-//     HSLAPixel px(40., 1.0, 0.5);
+    animation anim;
+    anim = filler::fillGridBFS(img, GRIDX, GRIDY, px, GRIDGRIDSPACING,
+                                     GRIDTOLERANCE, GRIDFRAMEFREQ);
+    PNG result = anim.write("images/bfsgrid.gif");
+    result.writeToFile("images/bfsgrid.png");
 
-//     animation anim;
-//     anim = filler::fillGridDFS(img, GRIDX, GRIDY, px, GRIDGRIDSPACING,
-//                                      GRIDTOLERANCE, GRIDFRAMEFREQ);
-//     PNG result = anim.write("images/dfsgrid.gif");
-//     result.writeToFile("images/dfsgrid.png");
+    PNG expected; expected.readFromFile("soln_images/bfsgrid.png");
+    REQUIRE(result==expected);
+}
 
-//     PNG expected; expected.readFromFile("soln_images/dfsgrid.png");
-//     REQUIRE(result==expected);
-// }
+TEST_CASE("fill::basic grad dfs","[weight=1][part=fill]"){
+    PNG img;
+    img.readFromFile(GRADIENTTESTIMAGE);
+    HSLAPixel px1;
+    px1.h = 0; px1.s = 1.0; px1.l = 0.5;
+    HSLAPixel px2;
+    px2.h = 200; px2.s = 1.0; px2.l = 0.5;
 
-// TEST_CASE("fill::basic grid bfs","[weight=1][part=fill]"){
-//     PNG img;
-//     img.readFromFile(GRIDTESTIMAGE);
-//     HSLAPixel px(200., 1.0, 0.5);
+    animation anim;
+    anim = filler::fillGradientDFS(img, GRADIENTX, GRADIENTY, px1, px2,
+                                         GRADIENTRADIUS, GRADIENTTOLERANCE,
+                                         GRADIENTFRAMEFREQ);
+    PNG result = anim.write("images/dfsgrad.gif");
+    result.writeToFile("images/dfsgrad.png");
 
-//     animation anim;
-//     anim = filler::fillGridBFS(img, GRIDX, GRIDY, px, GRIDGRIDSPACING,
-//                                      GRIDTOLERANCE, GRIDFRAMEFREQ);
-//     PNG result = anim.write("images/bfsgrid.gif");
-//     result.writeToFile("images/bfsgrid.png");
+    PNG expected; expected.readFromFile("soln_images/dfsgrad.png");
+    REQUIRE(result==expected);
+}
 
-//     PNG expected; expected.readFromFile("soln_images/bfsgrid.png");
-//     REQUIRE(result==expected);
-// }
+TEST_CASE("fill::basic grad bfs","[weight=1][part=fill]"){
+    PNG img;
+    img.readFromFile(GRADIENTTESTIMAGE);
+    HSLAPixel px1;
+    px1.h = 0; px1.s = 1.0; px1.l = 0.5;
+    HSLAPixel px2;
+    px2.h = 200; px2.s = 1.0; px2.l = 0.5;
 
-// TEST_CASE("fill::basic grad dfs","[weight=1][part=fill]"){
-//     PNG img;
-//     img.readFromFile(GRADIENTTESTIMAGE);
-//     HSLAPixel px1;
-//     px1.h = 0; px1.s = 1.0; px1.l = 0.5;
-//     HSLAPixel px2;
-//     px2.h = 200; px2.s = 1.0; px2.l = 0.5;
+    animation anim;
+    anim = filler::fillGradientBFS(img, GRADIENTX, GRADIENTY, px1, px2,
+                                         GRADIENTRADIUS, GRADIENTTOLERANCE,
+                                         GRADIENTFRAMEFREQ);
+    PNG result = anim.write("images/bfsgrad.gif");
+    result.writeToFile("images/bfsgrad.png");
 
-//     animation anim;
-//     anim = filler::fillGradientDFS(img, GRADIENTX, GRADIENTY, px1, px2,
-//                                          GRADIENTRADIUS, GRADIENTTOLERANCE,
-//                                          GRADIENTFRAMEFREQ);
-//     PNG result = anim.write("images/dfsgrad.gif");
-//     result.writeToFile("images/dfsgrad.png");
+    PNG expected; expected.readFromFile("soln_images/bfsgrad.png");
+    REQUIRE(result==expected);
+}
 
-//     PNG expected; expected.readFromFile("soln_images/dfsgrad.png");
-//     REQUIRE(result==expected);
-// }
+TEST_CASE("fill::basic rainbow dfs","[weight=1][part=fill]"){
 
-// TEST_CASE("fill::basic grad bfs","[weight=1][part=fill]"){
-//     PNG img;
-//     img.readFromFile(GRADIENTTESTIMAGE);
-//     HSLAPixel px1;
-//     px1.h = 0; px1.s = 1.0; px1.l = 0.5;
-//     HSLAPixel px2;
-//     px2.h = 200; px2.s = 1.0; px2.l = 0.5;
+    PNG img;
+    img.readFromFile(RAINTESTIMAGE);
 
-//     animation anim;
-//     anim = filler::fillGradientBFS(img, GRADIENTX, GRADIENTY, px1, px2,
-//                                          GRADIENTRADIUS, GRADIENTTOLERANCE,
-//                                          GRADIENTFRAMEFREQ);
-//     PNG result = anim.write("images/bfsgrad.gif");
-//     result.writeToFile("images/bfsgrad.png");
+    animation anim;
+    anim = filler::fillRainDFS(img, RAINX, RAINY, RAINFREQ,
+                                         RAINTOLERANCE,
+                                         RAINFRAMEFREQ);
+    PNG result = anim.write("images/dfsrain.gif");
+    result.writeToFile("images/dfsrain.png");
 
-//     PNG expected; expected.readFromFile("soln_images/bfsgrad.png");
-//     REQUIRE(result==expected);
-// }
+    PNG expected; expected.readFromFile("soln_images/dfsrain.png");
+    REQUIRE(result==expected);
 
-// TEST_CASE("fill::basic rainbow dfs","[weight=1][part=fill]"){
+}
+TEST_CASE("fill::basic rainbow bfs","[weight=1][part=fill]"){
 
-//     PNG img;
-//     img.readFromFile(RAINTESTIMAGE);
+    PNG img;
+    img.readFromFile(RAINTESTIMAGE);
 
-//     animation anim;
-//     anim = filler::fillRainDFS(img, RAINX, RAINY, RAINFREQ,
-//                                          RAINTOLERANCE,
-//                                          RAINFRAMEFREQ);
-//     PNG result = anim.write("images/dfsrain.gif");
-//     result.writeToFile("images/dfsrain.png");
+    animation anim;
+    anim = filler::fillRainBFS(img, RAINX, RAINY, RAINFREQ,
+                                         RAINTOLERANCE,
+                                         RAINFRAMEFREQ);
+    PNG result = anim.write("images/bfsrain.gif");
+    result.writeToFile("images/bfsrain.png");
 
-//     PNG expected; expected.readFromFile("soln_images/dfsrain.png");
-//     REQUIRE(result==expected);
+    PNG expected; expected.readFromFile("soln_images/bfsrain.png");
+    REQUIRE(result==expected);
 
-// }
-// TEST_CASE("fill::basic rainbow bfs","[weight=1][part=fill]"){
-
-//     PNG img;
-//     img.readFromFile(RAINTESTIMAGE);
-
-//     animation anim;
-//     anim = filler::fillRainBFS(img, RAINX, RAINY, RAINFREQ,
-//                                          RAINTOLERANCE,
-//                                          RAINFRAMEFREQ);
-//     PNG result = anim.write("images/bfsrain.gif");
-//     result.writeToFile("images/bfsrain.png");
-
-//     PNG expected; expected.readFromFile("soln_images/bfsrain.png");
-//     REQUIRE(result==expected);
-
-// }
+}
 
